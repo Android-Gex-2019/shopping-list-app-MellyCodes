fee23aefe9eb114740512f40f6bdedf8c7af2b2a
COFFEEISLIFE\melan <melanieroyplommer@gmail.com>
2019-02-09 20:07:35 -0400

    Added Comments

--- app/src/main/java/ca/nbcc/shoppinglist/MainActivity.java
+++ app/src/main/java/ca/nbcc/shoppinglist/MainActivity.java
@@ -1,3 +1,27 @@
+/**
+ * @file MainActivity.java
+ * @author Melanie Roy-Plommer
+ * @version 1.0
+ * @section DESCRIPTION
+ * <  >
+ * @section LICENSE
+ * Copyright 2018 - 2019
+ * Permission to use, copy, modify, and/or distribute this software for
+ * any purpose with or without fee is hereby granted, provided that the
+ * above copyright notice and this permission notice appear in all copies.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * @section Academic Integrity
+ * I certify that this work is solely my own and complies with
+ * NBCC Academic Integrity Policy (policy 1111)
+ */
+
 package ca.nbcc.shoppinglist;
 
 import android.content.Intent;
@@ -25,50 +49,48 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState != null) {
             Log.d(LOG_TAG, "SAVED INSTANCE");
             shoppingList = (ShoppingList) savedInstanceState.getSerializable("shoppingList");
-        }
-        else{
+        } else {
             Log.d(LOG_TAG, "NO SAVED INSTANCE");
-
         }
 
         showShoppingList();
     }
 
-    public void launchItemsActivity(View view){
+    public void launchItemsActivity(View view) {
 //        Log.d(LOG_TAG, "Main Activity: launchItemsActivity");
         Intent intent = new Intent(this, ItemsActivity.class);
         startActivityForResult(intent, TEXT_REQUEST);
 
     }
 
-    private void showShoppingList(){
+    private void showShoppingList() {
         Log.d(LOG_TAG, "Main Activity: showShoppingList");
 
         //clear fields
-        for(int i = 1; i <= 10; i++){
+        for (int i = 1; i <= 10; i++) {
             String field = "textViewItem" + i;
-            TextView tmpItem = (TextView)findViewById((int)getResources().getIdentifier(field, "id", getPackageName()));
+            TextView tmpItem = (TextView) findViewById(getResources().getIdentifier(field, "id", getPackageName()));
 
-            if(tmpItem != null){
+            if (tmpItem != null) {
                 tmpItem.setVisibility(View.INVISIBLE);
                 tmpItem.setText("");
             }
         }
 
         //Iterator with shoppingList map
-        Iterator<Map.Entry<String , Integer>> iter = shoppingList.getAllItems().entrySet().iterator();;
+        Iterator<Map.Entry<String, Integer>> iter = shoppingList.getAllItems().entrySet().iterator();
 
 
         int i = 1;
 
-        while(iter.hasNext()){
+        while (iter.hasNext()) {
             Map.Entry<String, Integer> item = iter.next();
             String field = "textViewItem" + i;
-            TextView tmpItem = (TextView)findViewById((int)getResources().getIdentifier(field, "id", getPackageName()));
+            TextView tmpItem = (TextView) findViewById(getResources().getIdentifier(field, "id", getPackageName()));
 
-            if(tmpItem != null){
+            if (tmpItem != null) {
                 tmpItem.setVisibility(View.VISIBLE);
-                tmpItem.setText(item.getKey()+ " -- "+ item.getValue());
+                tmpItem.setText(item.getKey() + " -- " + item.getValue());
             }
             i++;
         }
@@ -79,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-//        Log.d(LOG_TAG, "Main Activity: onResume");
+        Log.d(LOG_TAG, "Main Activity: onResume");
 
         showShoppingList();
     }
@@ -87,11 +109,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-//        Log.d(LOG_TAG, "Main Activity: onRestoreInstanceState");
+        Log.d(LOG_TAG, "Main Activity: onRestoreInstanceState");
 
         // get the list back from the Bundle(outState) saved in the onSavedInstanceState method
-        if(savedInstanceState != null){
-            shoppingList = (ShoppingList)savedInstanceState.getSerializable("shoppingList");
+        if (savedInstanceState != null) {
+            shoppingList = (ShoppingList) savedInstanceState.getSerializable("shoppingList");
 
         }
 
@@ -104,9 +126,9 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         Log.d(LOG_TAG, "Main Activity: onActivityResult");
 
-        if(requestCode == TEXT_REQUEST){
+        if (requestCode == TEXT_REQUEST) {
 
-            if(resultCode == RESULT_OK){
+            if (resultCode == RESULT_OK) {
                 String item = data.getStringExtra(ItemsActivity.EXTRA_REPLY);
 
                 shoppingList.addItem(item);
--- app/src/main/java/ca/nbcc/shoppinglist/ShoppingList.java
+++ app/src/main/java/ca/nbcc/shoppinglist/ShoppingList.java
@@ -1,3 +1,31 @@
+/**
+ * @file ShoppingList.java
+ * @author Melanie Roy-Plommer
+ * @version 1.0
+ *
+ * @section DESCRIPTION
+ * < ShoppingList class. Has methods to add new shopping items and
+ * return a Map of all items>
+ *
+ * @section LICENSE
+ * Copyright 2018 - 2019
+ * Permission to use, copy, modify, and/or distribute this software for
+ * any purpose with or without fee is hereby granted, provided that the
+ * above copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * @section Academic Integrity
+ * I certify that this work is solely my own and complies with
+ * NBCC Academic Integrity Policy (policy 1111)
+ */
+
 package ca.nbcc.shoppinglist;
 
 
@@ -12,6 +40,7 @@ public class ShoppingList implements Serializable {
     private static final String LOG_TAG = ShoppingList.class.getSimpleName();
     private Map itemsList = new HashMap();
 
+    // add new items to the shopping list or increment existing items
     public void addItem(String item){
         //itemsList.isEmpty() ||
         if(itemsList.containsKey(item)){
@@ -24,14 +53,11 @@ public class ShoppingList implements Serializable {
         }
     }
 
+    // Return all items
     public Map<String, Integer> getAllItems(){
         Log.d(LOG_TAG, "ShoppingList: Returning Map of items and count");
         return this.itemsList;
     }
 
-    public void clearItems(){
-        Log.d(LOG_TAG, "ShoppingList: clearItems");
-        this.itemsList.clear();
-    }
 
 }
